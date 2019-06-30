08423ef37cbf5598cf5383d198d0c7a965a88b9b
Gabriel Pivetti <gabriel.pivetti@centralar.com.br>
2019-06-30 18:37:43 -0300

    Pedidos de compra

--- classes/pedidosCompra.php
+++ classes/pedidosCompra.php
@@ -58,6 +58,7 @@ class PedidosCompra{
 
     public function getPedidosCompraInsumos($filters){
         $where = '';
+        
         if(count($filters) > 0){
             $where = 'where ';
             $i = 0;
@@ -66,28 +67,23 @@ class PedidosCompra{
                 if($key === 'id') {
                     $nick = 'pc.';
                     $equalBinding = ' = :'.$key;
-                } else if($key === 'statusPedido' or $key === 'status'){
-                    $nick = 'pc.';
-                    $equalBinding = ' = :'.$key;
-
-                    // Verifying statuses list
+                } else if($key === 'status' or $key === 'statusPedido' or $key === 'statusInsumo'){
+                    $nick = ($key === 'statusInsumo') ? 'pci' : 'pc';
                     $statusesArray = explode(',', $value);
-                    if($statusesArray > 1) {
-                        $equalBinding = ' in (:'.$key.')';
-                    }
-
-                    $key = 'status';
-                } else if($key === 'statusInsumo') {
-                    $nick = 'pci.';
-                    $equalBinding = ' = :'.$key;
-
-                    // Verifying statuses list
-                    $statusesArray = explode(',', $value);
-                    if($statusesArray > 1) {
-                        $equalBinding = ' in (:'.$key.')';
-                    } 
-                    
-                    $key = 'status';
+                    if(count($statusesArray) > 1) {
+                        $equalBinding = '';
+                        unset($filters[$key]);
+                        foreach($statusesArray as $key_status=>$value_status){
+                            if($key_status > 0) $equalBinding .= ' or ';
+                            $equalBinding .= $nick.'.status = :'.$key.$key_status;
+                            $filters[$key.$key_status] = $value_status;
+                        }
+                        $key = '';                        
+                        $nick = '';
+                    } else {
+                        $equalBinding = ' = :'.$key;
+                        $key = 'status';
+                    }                    
                 } else {
                     $nick = '';
                     $equalBinding = ' = :'.$key;
@@ -156,89 +152,7 @@ class PedidosCompra{
             'success' => true,
             'payload' => $responseData
         ));
-    }
-
-    public function getInsumosPedidosCompra($filters){
-        $where = '';
-        if(count($filters) > 0){
-            $where = 'where ';
-            $i = 0;
-            foreach($filters as $key => $value){
-                // Table's nickname
-                if($key === 'id' or $key === 'statusPedido' or $key === 'status')
-                    $nick = 'pc.';
-                else if($key === 'statusInsumo')
-                    $nick = 'pci.';
-                else
-                    $nick = '';
-
-                $and = $i > 0 ? ' and ' : '';
-                $where .= $and.$nick.$key.' = :'.$key;
-                $i++;
-            }
-        }
-
-        $sql = 'select 	pc.id, pc.dthr_pedido, pc.dt_prevista, pc.chave_nf, pc.status as statusPedido,
-                        pc.id_fornecedor as idFornecedor, f.nome as nomeFornecedor,
-                        pci.id_insumo as idInsumo, ins.nome as nomeInsumo, ins.ins, um.nome as nomeUnidadeMedida, 
-                        pci.status as statusInsumo, pci.quantidade, pci.quantidade_conferida, pci.dthr_recebimento, pci.local
-                from	pcp_pedidos pc
-                        inner join pcp_pedidos_insumos pci on pci.id_pedido = pc.id
-                        inner join pcp_insumos ins on pci.id_insumo = ins.id
-                        inner join pcp_fornecedores f on pc.id_fornecedor = f.id
-                        inner join pcp_unidades_medida um on um.id = ins.id_unidade_medida
-                '.$where.'
-                order by pc.dthr_pedido, pc.id, pci.id';
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute($filters);
-
-        $i = 0;
-        $pedidoId = 0;
-        $responseData = array();
-        while ($row = $stmt->fetch()) {
-            // Pedido
-            if ($pedidoId != (int) $row->id) {
-                $dthr_pedido = explode(' ', $row->dthr_pedido);
-                $responseData[] = array(
-                    'id'                => (int) $row->id,
-                    'data_pedido'       => (isset($dthr_pedido[0]) and $dthr_pedido[0])  ? $dthr_pedido[0] : null,
-                    'hora_pedido'       => (isset($dthr_pedido[1]) and $dthr_pedido[1])  ? $dthr_pedido[1] : null,
-                    'data_prevista'     => $row->dt_prevista,
-                    'idFornecedor'      => (int) $row->idFornecedor,
-                    'nomeFornecedor'    => $row->nomeFornecedor,
-                    'chave_nf'          => $row->chave_nf,
-                    'status'            => $row->statusPedido,
-                    'insumos'           => array()
-                );
-                $i++;
-            }
-
-            // Insumos
-            $dthr_pedido = explode(' ', $row->dthr_pedido);
-            $dthr_recebimento = explode(' ', $row->dthr_recebimento);
-            $responseData[($i-1)]['insumos'][] = array(
-                'id'                    => (int) $row->idInsumo,
-                'idPedido'              => $row->id,
-                'idItem'                => $row->nomeInsumo,                
-                'nome'                  => $row->nomeInsumo,
-                'ins'                   => $row->ins,
-                'unidademedida'         => $row->nomeUnidadeMedida,
-                'quantidade'            => (float) $row->quantidade,
-                'quantidade_conferida'  => (float) $row->quantidade_conferida,                                
-                'data_recebimento'      => (isset($dthr_recebimento[0]) and $dthr_recebimento[0]) ? $dthr_recebimento[0] : null,
-                'hora_recebimento'      => (isset($dthr_recebimento[1]) and $dthr_recebimento[1]) ? $dthr_recebimento[1] : null,
-                'local'                 => $row->local,
-                'status'                => $row->statusInsumo
-            );
-
-            $pedidoId = $row->id;
-        }
-
-        return json_encode(array(
-            'success' => true,
-            'payload' => $responseData
-        ));
-    }    
+    }  
 
     public function createUpdatePedidoCompra($request){
         try{
