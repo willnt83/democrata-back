07bedf2f5128135b447358d1e1816d3d37f27746
Gabriel Pivetti <gabriel.pivetti@centralar.com.br>
2019-07-04 21:45:32 -0300

    Pedido de compra

--- classes/pedidosCompra.php
+++ classes/pedidosCompra.php
@@ -67,8 +67,8 @@ class PedidosCompra{
                 if($key === 'id') {
                     $nick = 'pc.';
                     $equalBinding = ' = :'.$key;
-                } else if($key === 'status' or $key === 'statusPedido' or $key === 'statusInsumo'){
-                    $nick = ($key === 'statusInsumo') ? 'pci.' : 'pc.';
+                } else if($key === 'status' or $key === 'statusInsumo'){
+                    $nick = 'pci.';
                     $statusesArray = explode(',', $value);
                     if(count($statusesArray) > 1) {
                         $equalBinding = '';
@@ -95,17 +95,19 @@ class PedidosCompra{
             }
         }
 
-        $sql = 'select 	pc.id, pc.dthr_pedido, pc.dt_prevista, pc.chave_nf, pc.status as statusPedido,
-                        pc.id_fornecedor as idFornecedor, f.nome as nomeFornecedor,
-                        pci.id_insumo as idInsumo, ins.nome as nomeInsumo, ins.ins, um.nome as nomeUnidadeMedida, 
-                        pci.status as statusInsumo, pci.quantidade, pci.quantidade_conferida, pci.dthr_recebimento, pci.local
+        $sql = 'select 	pc.id, pc.dthr_pedido, pc.dt_prevista, pc.chave_nf, pc.id_fornecedor as idFornecedor, f.nome as nomeFornecedor,
+                        pci.id as item, pci.id_insumo as idInsumo, ins.nome as nomeInsumo, ins.ins, 
+                        um.nome as nomeUnidadeMedida, pci.status as statusInsumo, pci.dthr_recebimento, 
+                        pci.quantidade, ifnull(sum(entrada.entrada_quantidade),0) as quantidade_conferida
                 from	pcp_pedidos pc
                         inner join pcp_pedidos_insumos pci on pci.id_pedido = pc.id
                         inner join pcp_insumos ins on pci.id_insumo = ins.id
                         inner join pcp_fornecedores f on pc.id_fornecedor = f.id
-                        inner join pcp_unidades_medida um on um.id = ins.id_unidade_medida
+                        left join pcp_unidades_medida um on um.id = ins.id_unidade_medida
+                        left join pcp_insumos_entrada entrada on pci.id = entrada.id_pedido_insumo
                 '.$where.'
-                order by pc.dthr_pedido, pc.id, pci.id';
+                group by pci.id
+                order by pc.id, pci.id';
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute($filters);
 
@@ -124,7 +126,6 @@ class PedidosCompra{
                     'idFornecedor'      => (int) $row->idFornecedor,
                     'nomeFornecedor'    => $row->nomeFornecedor,
                     'chave_nf'          => $row->chave_nf,
-                    'statusPedido'      => $row->statusPedido,
                     'insumos'           => array()
                 );
                 $i++;
@@ -133,6 +134,7 @@ class PedidosCompra{
             // Insumos
             $dthr_recebimento = explode(' ', $row->dthr_recebimento);
             $responseData[($i-1)]['insumos'][] = array(
+                'item'                  => (int) $row->item,
                 'id'                    => (int) $row->idInsumo,
                 'nome'                  => $row->nomeInsumo,
                 'ins'                   => $row->ins,
@@ -141,7 +143,6 @@ class PedidosCompra{
                 'quantidade_conferida'  => (float) $row->quantidade_conferida,                                
                 'data_recebimento'      => (isset($dthr_recebimento[0]) and $dthr_recebimento[0]) ? $dthr_recebimento[0] : null,
                 'hora_recebimento'      => (isset($dthr_recebimento[1]) and $dthr_recebimento[1]) ? $dthr_recebimento[1] : null,
-                'local'                 => $row->local,
                 'statusInsumo'          => $row->statusInsumo
             );
 
