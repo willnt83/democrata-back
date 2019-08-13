3be7bb45a08e02c9cbfd50a4336240f3d35f222b
Gabriel Pivetti <gabriel.pivetti@centralar.com.br>
2019-08-12 22:31:49 -0300

    BD

--- public/index.php
+++ public/index.php
@@ -20,23 +20,25 @@ $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
 /*// Produção
-$config['db']['host']   = 'br982.hostgator.com.br';
-$config['db']['user']   = 'debora35_pcp';
-$config['db']['pass']   = 'PCPSenha123';
-$config['db']['dbname'] = 'debora35_pcp';
+$config['db']['host']   = 'br522.hostgator.com.br';
+$config['db']['user']   = 'gerado49_democra';
+$config['db']['pass']   = 'Pr0DD3m0Cr';
+$config['db']['dbname'] = 'gerado49_democrata';
 */
 
-/*// Homolog
-$config['db']['host']   = 'br982.hostgator.com.br';
-$config['db']['user']   = 'debora35_pcp';
-$config['db']['pass']   = 'PCPSenha123';
-$config['db']['dbname'] = 'debora35_homologdemocrata';
-*/
+// Homolog
+$config['db']['host']   = 'br522.hostgator.com.br';
+$config['db']['user']   = 'gerado49_hmdemoc';
+$config['db']['pass']   = 'D3m0Cr4t4Pass!@#';
+$config['db']['dbname'] = 'gerado49_hmdemocrata';
 
+
+/*// Local
 $config['db']['host']   = 'localhost';
 $config['db']['user']   = 'root';
 $config['db']['pass']   = 'asdzxcc11';
 $config['db']['dbname'] = 'pcp_prod';
+*/
 
 // Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
