a74035ecc3c05a70466d1fa9afbaa8050e17227c
Gabriel Pivetti <gabriel.pivetti@centralar.com.br>
2019-08-28 01:31:41 -0300

    correção import insumos

--- classes/insumos.php
+++ classes/insumos.php
@@ -136,122 +136,126 @@ class Insumos{
     }
 
     public function importInsumos($directory = '', $uploadedFiles = null){
-        try{
-            if($directory and $uploadedFiles){
-                require_once '../shared/UploadFile.php';
-            
-                $uploadedFile = $uploadedFiles['file'];
-                if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
-                    $filename = UploadFile::moveUploadedFile($directory, $uploadedFile);
-                    if($filename){
-                        $file = fopen($directory . DIRECTORY_SEPARATOR . $filename,"r");
-                        while(! feof($file)){
-                            $fileArray = fgetcsv($file);
+        if($directory and $uploadedFiles){
+            require_once '../shared/UploadFile.php';
+        
+            $uploadedFile = $uploadedFiles['file'];
+            if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
+                $filename = UploadFile::moveUploadedFile($directory, $uploadedFile);
+                if($filename){
+                    $linecont = 1;
+                    $file = fopen($directory . DIRECTORY_SEPARATOR . $filename,"r");
+                    while(!feof($file)){
+                        $fileArray = fgetcsv($file, 1000, '\t');
+                        if($fileArray and count($fileArray) > 0){
                             foreach($fileArray as $key=>$value){
-                                $content = explode(';', $value);
-                                if($content and count($content) >= 4){
-
-                                    // Verifica se já existe o insumo
-                                    if($content[0] !== '' and $content[0] and $content[1] !== '' and $content[1]){
-                                        $sqlInsumo = 'select count(*) as total from pcp_insumos p where LOWER(p.ins) = :ins and LOWER(p.nome) = :nome limit 1';
-                                        $stmtInsumo = $this->pdo->prepare($sqlInsumo);
-                                        $stmtInsumo->bindParam(':ins', strtolower(str_replace('INS-','',trim($content[0]))));
-                                        $stmtInsumo->bindParam(':nome', strtolower(trim($content[1])));
-                                        $stmtInsumo->execute();
-                                        $rowUnidade = $stmtInsumo->fetch();
-                                        $insertInsumo = ($rowUnidade->total and $rowUnidade->total > 0) ? false : true;
-                                    } else {
-                                        $insertInsumo = true;
-                                    }
-
-                                    if($insertInsumo){
-                                        // Retorna a unidade de medida
-                                        if($content[3] and $content[3] !== null){
-                                            $sqlUnidade = 'select id from pcp_unidades_medida where LOWER(unidade) = :unidade order by id desc limit 1';
-                                            $stmtUnidade = $this->pdo->prepare($sqlUnidade);
-                                            $stmtUnidade->bindParam(':unidade', strtolower($content[3]));
-                                            $stmtUnidade->execute();
-                                            $rowUnidade = $stmtUnidade->fetch();
-                                            $content[3] = ($rowUnidade->id) ? (int) $rowUnidade->id : null;
+                                try{
+                                    $content = explode(';', $value);
+                                    if($content and count($content) >= 4){
+                                        // Verifica se já existe o insumo
+                                        if($content[0] !== '' and $content[0] and $content[1] !== '' and $content[1]){
+                                            $sqlInsumo = '  select  count(*) as total 
+                                                            from    pcp_insumos p 
+                                                            where   LOWER(p.ins) COLLATE latin1_general_ci = LOWER(:ins) COLLATE utf8_general_ci and 
+                                                                    LOWER(p.nome) COLLATE latin1_general_ci = LOWER(:nome) COLLATE utf8_general_ci 
+                                                            limit   1';
+                                            $stmtInsumo = $this->pdo->prepare($sqlInsumo);
+                                            $stmtInsumo->bindParam(':ins', str_replace('INS-','',trim($content[0])));
+                                            $stmtInsumo->bindParam(':nome', trim($content[1]));
+                                            $stmtInsumo->execute();
+                                            $rowUnidade = $stmtInsumo->fetch();
+                                            $insertInsumo = ($rowUnidade->total and $rowUnidade->total > 0) ? false : true;
                                         } else {
-                                            $content[3] = null;
+                                            $insertInsumo = true;
                                         }
-                                        if($content[3] && $content[3] > 0){
-                                            $stmt = null;
 
-                                            $sql = '
-                                                insert into pcp_insumos
-                                                set
-                                                    nome = :nome,
-                                                    ins = :ins,
-                                                    ativo = "Y",
-                                                    categoria = :categoria,
-                                                    id_unidade_medida = :unidademedida,
-                                                    comprimento = :comprimento,
-                                                    largura = :largura,
-                                                    altura = :altura';
-                                            $stmt = $this->pdo->prepare($sql);
+                                        if($insertInsumo){
+                                            // Retorna a unidade de medida
+                                            if($content[3] and $content[3] !== null){
+                                                $sqlUnidade = 'select id from pcp_unidades_medida where LOWER(unidade) = :unidade order by id desc limit 1';
+                                                $stmtUnidade = $this->pdo->prepare($sqlUnidade);
+                                                $stmtUnidade->bindParam(':unidade', strtolower($content[3]));
+                                                $stmtUnidade->execute();
+                                                $rowUnidade = $stmtUnidade->fetch();
+                                                $content[3] = ($rowUnidade->id) ? (int) $rowUnidade->id : null;
+                                            } else {
+                                                $content[3] = null;
+                                            }
+                                            if($content[3] && $content[3] > 0){
+                                                $stmt = null;
 
-                                            if($content[0] === '' or $content[0] === null)
-                                                $stmt->bindParam(':ins', $n = null, PDO::PARAM_INT);
-                                            else
-                                                $stmt->bindParam(':ins', str_replace('INS-','',$content[0])); 
+                                                $sql = '
+                                                    insert into pcp_insumos
+                                                    set
+                                                        nome = :nome,
+                                                        ins = :ins,
+                                                        ativo = "Y",
+                                                        categoria = :categoria,
+                                                        id_unidade_medida = :unidademedida,
+                                                        comprimento = :comprimento,
+                                                        largura = :largura,
+                                                        altura = :altura';
+                                                $stmt = $this->pdo->prepare($sql);
 
-                                            if($content[1] === '' or $content[1] === null)
-                                                $stmt->bindParam(':nome', $n = null, PDO::PARAM_INT);
-                                            else
-                                                $stmt->bindParam(':nome', trim($content[1]));
+                                                if($content[0] === '' or $content[0] === null)
+                                                    $stmt->bindParam(':ins', $n = null);
+                                                else
+                                                    $stmt->bindParam(':ins', str_replace('INS-','',$content[0])); 
 
-                                            if($content[2] === '' or $content[2] === null)
-                                                $stmt->bindParam(':categoria', $n = null, PDO::PARAM_INT);
-                                            else
-                                                $stmt->bindParam(':categoria', trim($content[1]));
+                                                if($content[1] === '' or $content[1] === null)
+                                                    $stmt->bindParam(':nome', $n = null);
+                                                else
+                                                    $stmt->bindParam(':nome', trim($content[1]));
 
-                                            if($content[3] === '' or $content[3] === null or !is_numeric($content[3]))
-                                                $stmt->bindParam(':unidademedida', $n = null, PDO::PARAM_INT);
-                                            else
-                                                $stmt->bindParam(':unidademedida', $content[3]);
+                                                if($content[2] === '' or $content[2] === null)
+                                                    $stmt->bindParam(':categoria', $n = null);
+                                                else
+                                                    $stmt->bindParam(':categoria', trim($content[1]));
 
-                                            if(!array_key_exists(4, $content) or !isset($content[4]) or $content[4] === '' or $content[4] === null or !is_numeric($content[4]))
-                                                $stmt->bindParam(':comprimento', $n = null, PDO::PARAM_INT);
-                                            else
-                                                $stmt->bindParam(':comprimento', number_format($content[4],2,'.',''));
+                                                if($content[3] === '' or $content[3] === null or !is_numeric($content[3]))
+                                                    $stmt->bindParam(':unidademedida', $n = null, PDO::PARAM_INT);
+                                                else
+                                                    $stmt->bindParam(':unidademedida', $content[3]);
 
-                                            if(!array_key_exists(5, $content) or !isset($content[5]) or $content[5] === '' or $content[5] === null or !is_numeric($content[5]))
-                                                $stmt->bindParam(':largura', $n = null, PDO::PARAM_INT);
-                                            else
-                                                $stmt->bindParam(':largura', number_format($content[5],2,'.',''));
+                                                if(!array_key_exists(4, $content) or !isset($content[4]) or $content[4] === '' or $content[4] === null or !is_numeric($content[4]))
+                                                    $stmt->bindParam(':comprimento', $n = null);
+                                                else
+                                                    $stmt->bindParam(':comprimento', number_format($content[4],2,'.',''));
 
-                                            if(!array_key_exists(6, $content) or !isset($content[6]) or $content[6] === '' or $content[6] === null or !is_numeric($content[6]))
-                                                $stmt->bindParam(':altura', $n = null, PDO::PARAM_INT);
-                                            else
-                                                $stmt->bindParam(':altura', number_format($content[6],2,'.',''));                                            
+                                                if(!array_key_exists(5, $content) or !isset($content[5]) or $content[5] === '' or $content[5] === null or !is_numeric($content[5]))
+                                                    $stmt->bindParam(':largura', $n = null);
+                                                else
+                                                    $stmt->bindParam(':largura', number_format($content[5],2,'.',''));
 
-                                            $stmt->execute();
+                                                if(!array_key_exists(6, $content) or !isset($content[6]) or $content[6] === '' or $content[6] === null or !is_numeric($content[6]))
+                                                    $stmt->bindParam(':altura', $n = null);
+                                                else
+                                                    $stmt->bindParam(':altura', number_format($content[6],2,'.',''));                                            
+
+                                                $stmt->execute();
+                                            }
                                         }
                                     }
+                                } catch(\Exception $e) {
+                                    // Erro
                                 }
                             }
                         }
-                        fclose($file);
-
-                        return json_encode(array(
-                            'success' => true,
-                            'msg' => 'Insumos importados com sucesso!'
-                        ));                    
+                        $linecont++;
                     }
+                    fclose($file);
+
+                    return json_encode(array(
+                        'success' => true,
+                        'msg' => 'Insumos importados com sucesso!'
+                    ));                    
                 }
             }
-
-            return json_encode(array(
-                'success' => false,
-                'msg' => 'Não há arquivos válido para importar! Tente novamente.'
-            ));
-        }catch(\Exception $e){
-            return json_encode(array(
-                'success' => false,
-                'msg' => $e->getMessage()
-            ));
         }
+
+        return json_encode(array(
+            'success' => false,
+            'msg' => 'Não há arquivos válido para importar! Tente novamente.'
+        ));
     }
 }
